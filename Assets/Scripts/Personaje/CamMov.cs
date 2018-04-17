3a4e8e1eb05892cde4037c4689ec52f9100566d8
gambii007 <38330537+gambii007@users.noreply.github.com>
2018-04-17 16:55:35 -0500

    Add files via upload

---
+++ Assets/Scripts/Personaje/CamMov.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMov : MonoBehaviour {
+	
+	public GameObject personaje;
+	private Vector3 posicion;
+
+	// Use this for initialization
+	void Start () {
+		posicion = transform.position - personaje.transform.position;
+
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = personaje.transform.position+ posicion;
+	}
+}
---
+++ Assets/Scripts/Personaje/Mov.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov : MonoBehaviour {
+	public float rapidez = 5f;
+	public float salto = 250f;
+	public int b = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Movimiento();
+		/**if (Input.GetKey (KeyCode.RightArrow)) {
+			anim.SetInteger ("Estado", 2);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			anim.SetInteger ("Estado", 1);
+		}
+		if(Input.GetKey(KeyCode.UpArrow) && b==0){
+			anim.SetInteger ("Estado", 0);
+		}*/
+	}
+
+	void Movimiento(){
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (Vector2.right * rapidez * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate (Vector2.left * rapidez * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow) && b==0){
+			b=1;
+			GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, salto));
+			GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, 0));
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Suelo")){
+			b = 0;
+		}
+	}
+}
